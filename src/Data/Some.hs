b40afa45929b06e6cc7f01bf5a45795417b86b90
Patrick Thomson <patrickt@github.com>
2020-02-05 14:03:31 -0500

    Add Forall and fix some documentation.

--- src/Data/Some.hs
+++ src/Data/Some.hs
@@ -15,26 +15,38 @@
 --   data SomeShow = forall a . Show a => SomeShow
 -- @
 --
--- with the easier type @Some Show@.
+-- with the easier type @Some Show@. You can also wrap GADTs:
+--
+-- @
+--   data Number a where
+--     I :: Int -> Number Int
+--     F :: Float -> Number Int
+--
+--   type SomeNumber = Show1 ((~) Number) Forall
+-- @
+
 module Data.Some
   ( Some (..)
   , Some1 (..)
+  , Forall
   ) where
 
 import Data.Aeson
 import Data.Kind
-import Data.Proxy
 
--- | A polymorphic wrapper around a type of kind @*.
+-- | A polymorphic wrapper around a type of kind @*@.
 data Some (c :: Type -> Constraint) where
   Some :: c a => a -> Some c
+
+instance Show (Some Show) where
+  showsPrec n (Some a) = showsPrec n a
 
 instance ToJSON (Some ToJSON) where
   toJSON (Some a) = toJSON a
   toEncoding (Some a) = toEncoding a
 
 
--- | A polymorphic wrapper around a kind of parameterized type @* -> *@.
+-- | A polymorphic wrapper around a type taking a single parameter.
 -- The first constraint represents those rendered on the container @f@,
 -- and the second represents those on the constituent type @a@. If you
 -- don't need any constraints on @a@, use 'Forall'.
@@ -42,7 +54,8 @@ instance ToJSON (Some ToJSON) where
 -- The extra @k@ parameter to the constructor is to allow the kind of
 -- the parameterized type to be poly-kinded in the input type. This
 -- allows you to pass @(~) Proxy@ as the first argument and have some
--- type-level function as the second.
+-- type-level function as the second. In certain cases, you may need
+-- to specify it as 'Data.Kind.Type' with a type application.
 data Some1 c d where
   Some1 ::
     forall k
@@ -57,3 +70,9 @@ data Some1 c d where
 instance ToJSON (Some1 ToJSON1 ToJSON) where
   toJSON (Some1 a) = toJSON1 a
   toEncoding (Some1 a) = toEncoding1 a
+
+-- | A zero-methods typeclass used when you don't have any constraints
+-- you want to levy on some datum. Useful as the second parameter to
+-- Some1.
+class Forall (x :: k)
+instance Forall a
